b5c54d1bcaf98962a833ad692441aa07cedf7211
ToxFox88 <156932020+ToxFox88@users.noreply.github.com>
2024-02-13 16:55:39 +0300

    Update Makematerial.py

--- Pre-processing/Makematerial.py
+++ Pre-processing/Makematerial.py
@@ -5,3 +5,21 @@ ori = np.loadtxt('Cu_orient.txt')
 #Запись ориентаций из файла в переменную конфигурации
 config = damask.ConfigMaterial().material_add(O=damask.Rotation.from_quaternion(ori, accept_homomorph=True),phase='Cu',homogenization='SX') 
 print(f'configuration is{" " if config.is_valid else " not "}valid\n')
+#Запись параметров материала в переменную конфигурации
+config['phase']['Cu'] = damask.ConfigMaterial.load('CuNb.yaml')
+config['homogenization']['SX'] = {'N_constituents':1,'mechanical':{'type':'pass'}}
+config.save()
+config
+#Импорт ориентаций из файла
+ori = np.loadtxt('Nb_orient.txt') 
+#Запись ориентаций из файла в переменную конфигурации
+config = damask.ConfigMaterial().material_add(O=damask.Rotation.from_quaternion(ori, accept_homomorph=True),phase='Nb',homogenization='SX') 
+print(f'configuration is{" " if config.is_valid else " not "}valid\n')
+#Запись параметров материала в переменную конфигурации
+config['phase']['Nb'] = damask.ConfigMaterial.load('CuNb.yaml')
+#Ввыод на экран
+config
+config['homogenization']['SX'] = {'N_constituents':1,'mechanical':{'type':'pass'}}
+config.save()
+config.save()
+len(config['material'])
